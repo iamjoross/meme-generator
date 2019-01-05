f3a00671a484c9d589d214f2f9cd3dae0f850a46
Joross <iamjoross@gmail.com>
2019-01-05 23:51:04 +0800

    adde build folder

---
+++ build/precache-manifest.8e61add1a3fd12fde259da0193cfb009.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e7124701836a57c8835",
+    "url": "/static/css/main.66d30e56.chunk.css"
+  },
+  {
+    "revision": "6e7124701836a57c8835",
+    "url": "/static/js/main.6e712470.chunk.js"
+  },
+  {
+    "revision": "49131146e316c0964e86",
+    "url": "/static/js/1.49131146.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b7e62bdfdae91fef5a2b4f0909ddc19c",
+    "url": "/index.html"
+  }
+];
