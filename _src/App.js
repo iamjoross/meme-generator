82bae91294fad86a711c52121e81b0eae4fe2d7d
joross <joross.barredo@treatspace.com>
2019-01-05 23:38:25 +0800

    added sub folder

---
+++ _src/App.js
@@ -0,0 +1,54 @@
+import React, {Component} from "react"
+
+class App extends Component {
+
+    state = {
+        firstName: ""
+    }
+    
+    // Change to use arrow functions
+    handleChange = (event) => {
+        const { name, value } = event.target
+        this.setState({
+            [name]: value
+        })
+    }
+    
+    render() {
+        return (
+            <main>
+                <form>
+                    <input 
+                        type="text"
+                        name="firstName" 
+                        value={this.state.firstName} 
+                        onChange={this.handleChange} 
+                        placeholder="First Name"
+                    />
+                </form>
+                <h1>{this.state.firstName}</h1>
+            </main>
+        )
+    }
+}
+
+export default App
+
+
+/**
+ * Other modern/advanced React features/topics to learn:
+ * 
+ * Official React Context API - https://reactjs.org/docs/context.html
+ * Error Boundaries - https://reactjs.org/docs/error-boundaries.html
+ * render props - https://reactjs.org/docs/render-props.html
+ * Higher Order Components - https://reactjs.org/docs/higher-order-components.html
+ * React Router - https://reacttraining.com/react-router/core/guides/philosophy
+ * React Hooks - https://reactjs.org/docs/hooks-intro.html
+ * React lazy, memo, and Suspense - https://reactjs.org/blog/2018/10/23/react-v-16-6.html
+ */
+
+// https://medium.freecodecamp.org/every-time-you-build-a-to-do-list-app-a-puppy-dies-505b54637a5d
+
+// https://medium.freecodecamp.org/want-to-build-something-fun-heres-a-list-of-sample-web-app-ideas-b991bce0ed9a
+
+// https://medium.freecodecamp.org/summer-is-over-you-should-be-coding-heres-yet-another-list-of-exciting-ideas-to-build-a95d7704d36d
